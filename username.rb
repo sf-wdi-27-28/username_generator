5c569f8ba00cf9406e47e1af4628667c7a58d535
YOUR_GITHUB_USERNAME <jacquelynfambrough@gmail.com>
2016-04-25 15:55:35 -0700

    completed up to step 2 of level 2. saving progress

--- username.rb
+++ username.rb
@@ -2,13 +2,45 @@
 # Run `rspec spec/username_spec.rb` to get started.
 
 def format_name(first, last)
-  nil
+  username = first[0]
+  username << last
+  username.downcase
+
+  # end
+end
+format_name(" j a c k i e "," f a m b r o u g h ")
+
+def format_year year
+  year = year.to_s
+  if year.length == 4
+    year[-2,2]
+  end
 end
 
-def format_year
-  nil
+def build_username (first_name, last_name, year)
+  "#{format_name(first_name,last_name)}#{format_year(year)}"
 end
 
-def build_username
-  nil
+def check_privilege (number = 0)
+  if number == 0
+  "user"
+  elsif number == 1
+  "seller"
+  elsif number == 2
+  "manager"
+  else
+  "admin"
+  end
+end
+
+def user_type_prefix (level)
+  if level == 0
+  "user-"
+  elsif level == 1
+  "seller-"
+  elsif level == 2
+  "manager-"
+  else
+  "admin-"
+  end
 end
--- warmup.rb
+++ warmup.rb
@@ -1,17 +1,22 @@
 # Run `rspec spec/the_warmup_spec.rb` to get started.
 
 def say_hello
-  "hi"
+  "hello"
 end
 
 def scream(message)
-  message
+  message.upcase << "!"
 end
 
-def first_char
-  "z"
+def first_char(hooray)
+  first_letter = hooray[0]
+  first_letter.downcase
 end
 
-def polly_wanna
-  "crackercrackercracker"
+def polly_wanna (word)
+  word * 3
+end
+
+def after_you
+  "no, after you"
 end
